4ce73b7b36a30cd31039686ef961953089607bcc
liuwenhai <2179857632@qq.com>
2020-02-14 16:14:42 +0800

    测试三

--- src/java/test01.java
+++ src/java/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("刘文海");
         System.out.println("liuwenhai");
+        System.out.println("真不错");
     }
 }
